6626163c619b870170cff13e95fcc96ed2c2022c
hyunskki <76477352+gustjr7879@users.noreply.github.com>
2022-08-22 13:13:18 +0900

    Add files via upload

--- data.py
+++ data.py
@@ -4,6 +4,10 @@ import  networkx as nx
 import  scipy.sparse as sp
 from    scipy.sparse.linalg.eigen.arpack import eigsh
 import  sys
+from torch_geometric.datasets import Reddit,Flickr
+from sklearn.preprocessing import OneHotEncoder
+import os
+from scipy import stats
 
 
 def parse_index_file(filename):
@@ -28,7 +32,6 @@ def sample_mask(idx, l):
 def load_data(dataset_str):
     """
     Loads input data from gcn/data directory
-
     ind.dataset_str.x => the feature vectors of the training instances as scipy.sparse.csr.csr_matrix object;
     ind.dataset_str.tx => the feature vectors of the test instances as scipy.sparse.csr.csr_matrix object;
     ind.dataset_str.allx => the feature vectors of both labeled and unlabeled training instances
@@ -39,58 +42,94 @@ def load_data(dataset_str):
     ind.dataset_str.graph => a dict in the format {index: [index_of_neighbor_nodes]} as collections.defaultdict
         object;
     ind.dataset_str.test.index => the indices of test instances in graph, for the inductive setting as list object.
-
     All objects above must be saved using python pickle module.
-
     :param dataset_str: Dataset name
     :return: All data input files loaded (as well the training/test data).
     """
     names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
     objects = []
-    for i in range(len(names)):
-        with open("data/ind.{}.{}".format(dataset_str, names[i]), 'rb') as f:
-            if sys.version_info > (3, 0):
-                objects.append(pkl.load(f, encoding='latin1'))
-            else:
-                objects.append(pkl.load(f))
+    if dataset_str != 'flickr':
 
-    x, y, tx, ty, allx, ally, graph = tuple(objects)
-    test_idx_reorder = parse_index_file("data/ind.{}.test.index".format(dataset_str))
-    test_idx_range = np.sort(test_idx_reorder)
+        for i in range(len(names)):
+            with open("data/ind.{}.{}".format(dataset_str, names[i]), 'rb') as f:
+                if sys.version_info > (3, 0):
+                    objects.append(pkl.load(f, encoding='latin1'))
+                else:
+                    objects.append(pkl.load(f))
 
-    if dataset_str == 'citeseer':
-        # Fix citeseer dataset (there are some isolated nodes in the graph)
-        # Find isolated nodes, add them as zero-vecs into the right position
-        test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder)+1)
-        tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
-        tx_extended[test_idx_range-min(test_idx_range), :] = tx
-        tx = tx_extended
-        ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
-        ty_extended[test_idx_range-min(test_idx_range), :] = ty
-        ty = ty_extended
+        x, y, tx, ty, allx, ally, graph = tuple(objects)
+        test_idx_reorder = parse_index_file("data/ind.{}.test.index".format(dataset_str))
+        test_idx_range = np.sort(test_idx_reorder)
 
-    features = sp.vstack((allx, tx)).tolil()
-    features[test_idx_reorder, :] = features[test_idx_range, :]
-    adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
+        if dataset_str == 'citeseer':
+            # Fix citeseer dataset (there are some isolated nodes in the graph)
+            # Find isolated nodes, add them as zero-vecs into the right position
+            test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder)+1)
+            tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
+            tx_extended[test_idx_range-min(test_idx_range), :] = tx
+            tx = tx_extended
+            ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
+            ty_extended[test_idx_range-min(test_idx_range), :] = ty
+            ty = ty_extended
 
-    labels = np.vstack((ally, ty))
-    labels[test_idx_reorder, :] = labels[test_idx_range, :]
+        features = sp.vstack((allx, tx)).tolil()
+        features[test_idx_reorder, :] = features[test_idx_range, :]
+        adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
 
-    idx_test = test_idx_range.tolist()
-    idx_train = range(len(y))
-    idx_val = range(len(y), len(y)+500)
+        labels = np.vstack((ally, ty))
+        labels[test_idx_reorder, :] = labels[test_idx_range, :]
 
-    train_mask = sample_mask(idx_train, labels.shape[0])
-    val_mask = sample_mask(idx_val, labels.shape[0])
-    test_mask = sample_mask(idx_test, labels.shape[0])
+        idx_test = test_idx_range.tolist()
+        idx_train = range(len(y))
+        idx_val = range(len(y), len(y)+500)
 
-    y_train = np.zeros(labels.shape)
-    y_val = np.zeros(labels.shape)
-    y_test = np.zeros(labels.shape)
-    y_train[train_mask, :] = labels[train_mask, :]
-    y_val[val_mask, :] = labels[val_mask, :]
-    y_test[test_mask, :] = labels[test_mask, :]
+        train_mask = sample_mask(idx_train, labels.shape[0])
+        val_mask = sample_mask(idx_val, labels.shape[0])
+        test_mask = sample_mask(idx_test, labels.shape[0])
 
+        y_train = np.zeros(labels.shape)
+        y_val = np.zeros(labels.shape)
+        y_test = np.zeros(labels.shape)
+        y_train[train_mask, :] = labels[train_mask, :]
+        y_val[val_mask, :] = labels[val_mask, :]
+        y_test[test_mask, :] = labels[test_mask, :]
+    elif dataset_str == 'flickr':
+        dataset = Flickr
+
+        dataset = dataset('flickr/data')
+
+
+        data = dataset[0]
+        features = data.x
+        features = features.numpy()
+        raw_labels = data.y
+        labels = data.y
+        labels = np.array(labels).reshape(-1,1)
+        encoder = OneHotEncoder()
+        encoder.fit(labels)
+        labels = encoder.transform(labels)
+        labels = labels.toarray()
+
+        adj = sp.load_npz(os.path.join('flickr/data/raw','adj_full.npz'))
+        graph = nx.from_scipy_sparse_matrix(adj)
+        graph = nx.to_dict_of_lists(graph)
+
+        adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
+    
+        
+        idx_train = list(range(10000))
+        idx_val = range(len(idx_train),len(idx_train)+5000)
+        idx_test = list(range(len(idx_train)+len(idx_val),len(idx_train)+len(idx_val)+5000))
+        train_mask = sample_mask(idx_train, labels.shape[0])
+        val_mask = sample_mask(idx_val, labels.shape[0])
+        test_mask = sample_mask(idx_test, labels.shape[0])
+        y_train = np.zeros(labels.shape)
+        y_val = np.zeros(labels.shape)
+        y_test = np.zeros(labels.shape)
+        y_train[train_mask, :] = labels[train_mask, :]
+        y_val[val_mask, :] = labels[val_mask, :]
+        y_test[test_mask, :] = labels[test_mask, :]
+        features = sp.lil_matrix(features)      
     return adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask
 
 
--- train.py
+++ train.py
@@ -8,75 +8,76 @@ from    data import load_data, preprocess_features, preprocess_adj
 from    model import GCN
 from    config import  args
 from    utils import masked_loss, masked_acc
-
-
+import time
+'''
 seed = 123
 np.random.seed(seed)
 torch.random.manual_seed(seed)
+'''
+
+for i in range(100):
+    adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask= load_data(args.dataset) # adj is new adj , graph is new graph
+
+    #G = nx.from_dict_of_lists(new_graph)
+    #G.remove_edges_from(nx.selfloop_edges(G)) 
+    features = preprocess_features(features) # [49216, 2], [49216], [2708, 1433]
+    supports = preprocess_adj(adj)
+
+    device = torch.device('cuda')
+    train_label = torch.from_numpy(y_train).long().to(device)
+    num_classes = train_label.shape[1]
+    train_label = train_label.argmax(dim=1)
+    train_mask = torch.from_numpy(train_mask.astype(np.int32)).to(device)
+    val_label = torch.from_numpy(y_val).long().to(device)
+    val_label = val_label.argmax(dim=1)
+    val_mask = torch.from_numpy(val_mask.astype(np.int32)).to(device)
+    test_label = torch.from_numpy(y_test).long().to(device)
+    test_label = test_label.argmax(dim=1)
+    test_mask = torch.from_numpy(test_mask.astype(np.int32)).to(device)
+
+    i = torch.from_numpy(features[0]).long().to(device)
+    v = torch.from_numpy(features[1]).to(device)
+    feature = torch.sparse.FloatTensor(i.t(), v, features[2]).float().to(device)
+
+    i = torch.from_numpy(supports[0]).long().to(device)
+    v = torch.from_numpy(supports[1]).to(device)
+    support = torch.sparse.FloatTensor(i.t(), v, supports[2]).float().to(device)
+
+    #print('x :', feature)
+    #print('sp:', support)
+    num_features_nonzero = feature._nnz()
+    feat_dim = feature.shape[1]
 
 
-# load data
-adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask = load_data(args.dataset)
-print('adj:', adj.shape)
-print('features:', features.shape)
-print('y:', y_train.shape, y_val.shape, y_test.shape)
-print('mask:', train_mask.shape, val_mask.shape, test_mask.shape)
+    net = GCN(feat_dim, num_classes, num_features_nonzero)
+    net.to(device)
+    optimizer = optim.Adam(net.parameters(), lr=args.learning_rate)
 
-# D^-1@X
-features = preprocess_features(features) # [49216, 2], [49216], [2708, 1433]
-supports = preprocess_adj(adj)
+    start = time.time()
+    net.train()
+    for epoch in range(args.epochs):
 
-device = torch.device('cuda')
-train_label = torch.from_numpy(y_train).long().to(device)
-num_classes = train_label.shape[1]
-train_label = train_label.argmax(dim=1)
-train_mask = torch.from_numpy(train_mask.astype(np.int)).to(device)
-val_label = torch.from_numpy(y_val).long().to(device)
-val_label = val_label.argmax(dim=1)
-val_mask = torch.from_numpy(val_mask.astype(np.int)).to(device)
-test_label = torch.from_numpy(y_test).long().to(device)
-test_label = test_label.argmax(dim=1)
-test_mask = torch.from_numpy(test_mask.astype(np.int)).to(device)
+        out = net((feature, support))
+        out = out[0]
+        loss = masked_loss(out, train_label, train_mask)
+        loss += args.weight_decay * net.l2_loss()
 
-i = torch.from_numpy(features[0]).long().to(device)
-v = torch.from_numpy(features[1]).to(device)
-feature = torch.sparse.FloatTensor(i.t(), v, features[2]).to(device)
+        acc = masked_acc(out, train_label, train_mask)
 
-i = torch.from_numpy(supports[0]).long().to(device)
-v = torch.from_numpy(supports[1]).to(device)
-support = torch.sparse.FloatTensor(i.t(), v, supports[2]).float().to(device)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-print('x :', feature)
-print('sp:', support)
-num_features_nonzero = feature._nnz()
-feat_dim = feature.shape[1]
+        #if epoch % 10 == 0:
 
+            #print(epoch, loss.item(), acc.item())
 
-net = GCN(feat_dim, num_classes, num_features_nonzero)
-net.to(device)
-optimizer = optim.Adam(net.parameters(), lr=args.learning_rate)
-
-net.train()
-for epoch in range(args.epochs):
+    net.eval()
+    end = time.time()
+    check_time = end - start
 
     out = net((feature, support))
     out = out[0]
-    loss = masked_loss(out, train_label, train_mask)
-    loss += args.weight_decay * net.l2_loss()
+    acc = masked_acc(out, test_label, test_mask)
+    print('test:', acc.item(),'time:',check_time)
 
-    acc = masked_acc(out, train_label, train_mask)
-
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-    if epoch % 10 == 0:
-
-        print(epoch, loss.item(), acc.item())
-
-net.eval()
-
-out = net((feature, support))
-out = out[0]
-acc = masked_acc(out, test_label, test_mask)
-print('test:', acc.item())
